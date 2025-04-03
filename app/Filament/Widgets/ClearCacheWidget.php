3c575b82bc7dd8e923fafaaec25c6235010c37b1
nikelioum <dimliarop@gmail.com>
2025-04-03 20:34:46 +0300

    Mass delete activity logs

--- app/Filament/Widgets/ClearCacheWidget.php
+++ app/Filament/Widgets/ClearCacheWidget.php
@@ -5,10 +5,11 @@ namespace App\Filament\Widgets;
 use Filament\Widgets\Widget;
 use Filament\Notifications\Notification;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 class ClearCacheWidget extends Widget
 {
-    protected static ?int $sort = 1;
+    protected static ?int $sort = 2;
     protected static string $view = 'filament.widgets.clear-cache-widget';
 
     public function clearConfigCache()
@@ -31,6 +32,18 @@ class ClearCacheWidget extends Widget
         // Show a success notification after clearing views cache
         Notification::make()
             ->title('Views Cache Cleared')
+            ->success()
+            ->send();
+    }
+
+
+    public function deleteActivityLogs(){
+         
+        DB::table('activity_log')->truncate();
+
+        // Show a success notification after clearing views cache
+        Notification::make()
+            ->title('Activity Logs Cleared')
             ->success()
             ->send();
     }
--- resources/views/filament/widgets/clear-cache-widget.blade.php
+++ resources/views/filament/widgets/clear-cache-widget.blade.php
@@ -1,9 +1,10 @@
 <x-filament-widgets::widget>
     <x-filament::section>
-        <h2 class="text-lg font-semibold">Clear Cache</h2>
+        <h2 class="text-lg font-semibold">Shortcuts</h2>
         <div class="mt-4 flex" style="padding-top: 20px;">
             <x-filament::button wire:click="clearConfigCache" color="warning" style="margin-right:20px;">Clear Config Cache</x-filament::button>
-            <x-filament::button wire:click="clearViewsCache" color="warning">Clear Views Cache</x-filament::button>
+            <x-filament::button wire:click="clearViewsCache" color="warning" style="margin-right:20px;">Clear Views Cache</x-filament::button>
+            <x-filament::button wire:click="deleteActivityLogs" color="warning">Delete Activity Logs</x-filament::button>
         </div>
     </x-filament::section>
 </x-filament-widgets::widget>
